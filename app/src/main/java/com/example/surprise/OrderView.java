fac6db300e3925f85c255f7e7e325a7b55c85454
itzmbm <manjunathbm438@gmail.com>
2021-12-11 07:52:23 +0530

    added payment,order,profile ui

--- app/src/main/java/com/example/surprise/OrderView.java
+++ app/src/main/java/com/example/surprise/OrderView.java
@@ -2,24 +2,19 @@ package com.example.surprise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import java.util.Objects;
 
-public class cart extends AppCompatActivity {
+public class OrderView extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
-
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Cart");
+        setContentView(R.layout.activity_order_view);
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Orders");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E7879A")));
-
-
-
     }
 }
--- app/src/main/java/com/example/surprise/Profile.java
+++ app/src/main/java/com/example/surprise/Profile.java
@@ -2,24 +2,20 @@ package com.example.surprise;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
 import java.util.Objects;
 
-public class cart extends AppCompatActivity {
+public class Profile extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cart);
+        setContentView(R.layout.activity_profile);
 
-        Objects.requireNonNull(getSupportActionBar()).setTitle("Cart");
+        Objects.requireNonNull(getSupportActionBar()).setTitle("Profile");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E7879A")));
-
-
-
     }
 }
