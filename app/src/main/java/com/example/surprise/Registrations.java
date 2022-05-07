eed71873047f5b3747f3570763c40473a6ccec7e
Manjunath B M <manjunathbm438@gmail.com>
2022-05-07 22:04:33 +0530

    fixes

--- app/src/main/java/com/example/surprise/Registrations.java
+++ app/src/main/java/com/example/surprise/Registrations.java
@@ -7,15 +7,18 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 
 import java.util.Objects;
 
 public class Registrations extends AppCompatActivity {
 Button rg;
+EditText email,password,repassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,11 +29,16 @@ Button rg;
         window.setStatusBarColor(ContextCompat.getColor(Registrations.this, R.color.black));
         Objects.requireNonNull(getSupportActionBar()).setTitle("Registration");
         getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#E7879A")));
+        email=findViewById(R.id.em);
+        password=findViewById(R.id.pass);
+        repassword=findViewById(R.id.repas);
         rg=(Button) findViewById(R.id.register);
         rg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Intent i=new Intent(getApplicationContext(), Login.class);
+                startActivity(i);
+                finish();
             }
         });
 
